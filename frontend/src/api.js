bbe5d61fa51b5d4786ab64b31db4ad32cd427ed6
Sanandiya Yash <153787039+sanandiyayash@users.noreply.github.com>
2024-09-07 15:29:34 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_URL = 'https://minor-project-backend-rfsf.onrender.com';
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://minor-project-backend-rfsf.onrender.com';
+//const API_URL = 'http://localhost:3000';
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
